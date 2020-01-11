8a240571b60a7af8193d4ddd1bbb147f259523d0
manojsigh <manojsingh.manoj@gmail.com>
2020-01-11 18:28:13 +0530

    modifed hostname

--- src/main/java/com/boot/hello/controllers/HelloController.java
+++ src/main/java/com/boot/hello/controllers/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@RequestMapping("/hello")
 	public String greetings() {
-		return "Hello there! How are you?";
+		return "Hello there! How are you?.....   " + System.getenv("HOSTNAME");
 	}
 
 }
