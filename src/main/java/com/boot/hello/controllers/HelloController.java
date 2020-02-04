71b6ba9480f12f290688db1ab41c386848297c5f
ASHWANIVERMA <ashwani.verma@in.ibm.com>
2020-02-04 19:50:27 +0530

    updated probes in controller

--- src/main/java/com/boot/hello/controllers/HelloController.java
+++ src/main/java/com/boot/hello/controllers/HelloController.java
@@ -3,10 +3,12 @@ package com.boot.hello.controllers;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
 import java.io.StringReader;
-
+import java.util.Properties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -63,4 +65,54 @@ public class HelloController {
             
         return sb.toString();
     }
+
+    	public ResponseEntity<?> checkHealth(){
+		String message=null;
+		HttpStatus status;
+		if (healthy) {
+			message = "ping /healthz --> pong [healthy]";
+			status = HttpStatus.OK;
+		}else {
+			message = "ping /healthz => pong [unhealthy]";
+			status = HttpStatus.SERVICE_UNAVAILABLE;
+		}
+		System.out.println(message);
+		return new ResponseEntity<String>(message,status);
+    }
+	@RequestMapping("/ready")
+	public ResponseEntity<?> checkReadiness(){
+		String message=null;
+		HttpStatus status;
+		long now =(System.currentTimeMillis() / 1000) % 60;
+		if (now > 30) {
+			 message = "ping /ready => pong [ready]";
+			status = HttpStatus.OK;
+		}else {
+			message = "ping /ready => pong [notready]\n Error! Service not ready for requests...\\n";
+			status = HttpStatus.SERVICE_UNAVAILABLE;
+		}
+		System.out.println(message);
+		return new ResponseEntity<String>(message,status);
+    }
+	@RequestMapping("/flip")
+	public ResponseEntity<?> flip(@RequestParam("op") String flag){
+		String message=null;
+		 if (flag == "kill") {
+			  message = "Received kill request. Changing app state to unhealthy...\n Switched app state to unhealthy...\\n";
+			  healthy = false;
+			}
+			else if (flag == "awaken") {
+			  message ="Received awaken request. Changing app state to healthy...\n Switched app state to healthy...\n";
+			  healthy = true;
+			}
+		System.out.println(message);
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+    }
+
+    boolean healthy=true;
+	long started;
+	public HelloController() {
+		// TODO Auto-generated constructor stub
+		started = System.currentTimeMillis();
+	}
 }
